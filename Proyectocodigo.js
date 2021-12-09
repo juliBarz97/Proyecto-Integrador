63e082eb3bc2d373e2c60d619d5fcbb10898c88a
julian <julianbarz97@gmail.com>
2021-12-09 19:56:08 -0300

    cambios

---
+++ Proyectocodigo.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+
+
+app.listen(3000, () => console.log("Hola"));
+
+
+app.get('/', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+app.use('/public/', express.static(__dirname + '/public/'))
