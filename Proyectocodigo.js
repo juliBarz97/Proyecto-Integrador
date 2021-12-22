d539249695cbaf4fa62fa5ed57a6d546ce5fab81
julian <julianbarz97@gmail.com>
2021-12-22 17:19:55 -0300

    CAMBIOS IMPORTANTES

--- Proyectocodigo.js
+++ Proyectocodigo.js
@@ -10,4 +10,26 @@ app.get('/', function(req, res){
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
 
+
+app.get('/home.html', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+app.get('/Carrito.html', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/Carrito.html'));
+})
+
+app.get('/index.html', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+})
+
+app.get('/register.html', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
+app.get('/login.html', function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
+
+
 app.use('/public/', express.static(__dirname + '/public/'))
