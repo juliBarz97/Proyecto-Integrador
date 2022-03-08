a1a40b8391090362a87513b62231b632ec61e298
julian <julianbarz97@gmail.com>
2022-03-08 20:06:49 -0300

    models-usuarios arreglo S6

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,63 +1,64 @@
 'use strict';
 
-const db = require('../models/index');
+function usuarios(sequelize, DataTypes){
 
-//const op = db.Sequelize.Op;
-const usuarios = (sequelize, DataTypes) => {
-	const usuario = sequelize.define(
-		'usuario',
-		(cols = {
-			id: {
-				as: 'primaryKey',
-				primaryKey: true,
-			},
-			nombre: {
-				type: DataTypes.STRING(200),
-			},
-			email: {
-				type: DataTypes.STRING,
-				validate: {
-					isEmail: true,
-				},
-			},
-			password: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			domicilio: {
-				type: DataTypes.STRING,
-			},
-			perfil: {
-				type: DataTypes.STRING,
-			},
-			accesorios: {
-				type: DataTypes.STRING,
-			},
-		}),
-		(config = {
-			camelCase: false,
-			tablename: 'usuario',
-			timestamps: {
-				createAt: {
-					type: DataTypes.DATE,
-				},
-				updatedAt: {
-					type: DataTypes.DATE,
-				},
-			},
-		})
-	);
-	return usuario;
-};
 
-usuario.associate = function (models) {
-	usuario.hasMany(models.ventas, {
-		foreingKey: 'usuario_id', // foreing key de la tabla de ventas
-		as: 'ventas',
-	});
-	usuario.hasMany(models.producto, {
-		foreingKey: ' usuario_id', // foreing key de la tabla de producto
-		as: 'ventas',
-	});
+    const usuario = sequelize.define(
+        'usuario',
+        (cols = {
+            id: {
+                as: 'primaryKey',
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+            },
+            nombre: {
+                type: DataTypes.STRING(200),
+            },
+            email: {
+                type: DataTypes.STRING,
+                validate: {
+                    isEmail: true,
+                },
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            domicilio: {
+                type: DataTypes.STRING,
+            },
+            perfil: {
+                type: DataTypes.STRING,
+            },
+            accesorios: {
+                type: DataTypes.STRING,
+            },
+        }),
+        (config = {
+            camelCase: false,
+            tablename: 'usuario',
+            timestamps: {
+                createAt: {
+                    type: DataTypes.DATE,
+                },
+                updatedAt: {
+                    type: DataTypes.DATE,
+                },
+            },
+        })
+    );
+
+    usuario.associate = function (models) {
+        /*usuario.hasMany(models.ventas, {
+            foreingKey: 'usuario_id', // foreing key de la tabla de ventas
+            as: 'ventas',
+        });*/
+        usuario.hasMany(models.productos, {
+            foreingKey: ' usuario_id', // foreing key de la tabla de producto
+            as: 'ventas',
+        });
+    };
+
+    return usuario;
 };
 module.exports = usuarios;
